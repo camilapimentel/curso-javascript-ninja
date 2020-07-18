3175e4d24563b7aec1f3a672242c58f4e9aac8d9
Camila Pimentel <45701839+camilapimentel@users.noreply.github.com>
2020-07-18 17:05:34 +0100

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var numeros = [1, 2, 3 ,4, 5];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function retornaArr(arr) {
+   return arr;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(retornaArr(numeros[1]));
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,21 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function retornaParam(arr, num) {
+  num = arr[0];
+  return arr[0];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arrCinco = ['nome', 25, true, null, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+retornaArr(arrCinco);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +52,62 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nome) {
+    var livros = {
+          crepusculo: {
+          quantidadePaginas: 100,
+          autor: 'Stephenie Meyer 1',
+          editora: '	Little, Brown and Company 1'
+        },
+        eclipse: {
+           quantidadePaginas: 200,
+           autor: 'Stephenie Meyer 2',
+           editora: '	Little, Brown and Company 2'
+        },
+        amanhecer: {
+           quantidadePaginas: 300,
+           autor: 'Stephenie Meyer 3',
+           editora: '	Little, Brown and Company 3'
+        }
+    }
+  
+  if (nome === undefined) {
+    return livros
+  };
+  
+  if (nome === 'crepusculo') {
+    return livros.crepusculo;
+  };
+
+  if (nome === 'eclipse') {
+    return livros.eclipse;
+  };
+
+  if (nome === 'amanhecer') {
+    return livros.amanhecer;
+  };
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
-
+console.log(book());
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( 'O livro ' +  book('crepusculo').quantidadePaginas );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
-
+console.log( 'O autor do livro é ' +  book('crepusculo').autor );
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro foi publicado pela editora ' +  book('crepusculo').editora );
